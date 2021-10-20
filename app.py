34335a8895f9e4177dedd7c4a6794290ea909427
deborasantos28 <debora_hardy@hotmail.com>
2021-10-20 20:00:57 +0000

    Add dropdown menu for the game genres available to review on the form

--- app.py
+++ app.py
@@ -102,7 +102,8 @@ def sign_out():
 
 @app.route("/add_review")
 def add_review():
-    return render_template("add_review.html")
+    genres = mongo.db.genres.find().sort("genre_type", 1)
+    return render_template("add_review.html", genres=genres)
 
 
 if __name__ == "__main__":
